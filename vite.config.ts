2a250dc78a4925e78ea186ad8dc505d0d06b6274
Diogo Carvalho <diogofelipec@gmail.com>
2024-12-13 14:39:07 +0000

    fix: missing test in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -10,10 +9,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setUpTests.jsx',
   },
 })
