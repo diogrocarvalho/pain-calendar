f05d9ef5b7c21c1cfa2a32e487e144f1564cee0c
Diogo Carvalho <diogofelipec@gmail.com>
2024-12-13 18:27:19 +0000

    adding color to selected dates

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { PainList } from './components/pain-list/pain-list.component'
 import { Calendar } from './components/ui/calendar'
 import { Home } from './pages/home.page'
 import { Pain } from './types/pain'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+
+dayjs.extend(customParseFormat)
 
 function App() {
   const [selectedDate, setSelectedDate] = React.useState<Date | undefined>(
@@ -20,9 +23,17 @@ function App() {
     const storagedData = JSON.parse(localStorage.getItem('pains') || '{}')
     const currentPains =
       storagedData[dayjs(selectedDate).format('DD-MM-YYYY')] || []
+
+    const date = dayjs(selectedDate)
+    const now = dayjs()
+    const updatedDate = date
+      .set('hour', now.hour())
+      .set('minute', now.minute())
+      .set('second', now.second())
+
     const newPain = {
       ...pain,
-      time: dayjs().toDate(),
+      time: updatedDate.toDate(),
       id: new Date().getTime(),
     }
     currentPains.push(newPain)
@@ -30,6 +41,7 @@ function App() {
     localStorage.setItem('pains', JSON.stringify(storagedData))
     setCurrentPains(currentPains)
   }
+
   function deletePain(pain: Pain) {
     const storagedData = JSON.parse(localStorage.getItem('pains') || '{}')
     const currentPains =
@@ -39,6 +51,20 @@ function App() {
     localStorage.setItem('pains', JSON.stringify(storagedData))
     setCurrentPains(newPains)
   }
+
+  const getBookedDays = React.useCallback(() => {
+    const storagedData = JSON.parse(localStorage.getItem('pains') || '{}')
+    const bookedDays = Object.entries(storagedData)
+      .filter(([, value]) => {
+        if (Array.isArray(value)) {
+          return value.length > 0
+        }
+        return false
+      })
+      .map(([value]) => dayjs(value, 'DD-MM-YYYY').toDate())
+
+    return bookedDays
+  }, [])
 
   useEffect(() => {
     const storagedData = JSON.parse(localStorage.getItem('pains') || '{}')
@@ -52,6 +78,7 @@ function App() {
       <h1 className="text-2xl mb-4">Olá, Raquel!</h1>
       <div className="w-full flex flex-col items-center my-4">
         <Calendar
+          modifiers={{ booked: getBookedDays() }}
           className="max-w-64 rounded-lg border"
           mode="single"
           onSelect={setSelectedDate}
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
+import * as React from 'react'
 import { DayPicker } from 'react-day-picker'
 
-import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -17,6 +17,9 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
+      modifiersClassNames={{
+        booked: 'bg-pink-200 border border-pink-700',
+      }}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4',
